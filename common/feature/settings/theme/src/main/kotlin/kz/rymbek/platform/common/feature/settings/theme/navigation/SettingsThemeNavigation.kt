3abf4139b5cb8d7b862308945216ad6693ba9dca
Rymbek Kimanov <91866986+Rym-bek@users.noreply.github.com>
2025-11-24 08:42:56 +0500

    Feat: Add navigation extension for settings theme screen
    This commit introduces a new extension function to facilitate navigation to the settings theme screen.
    
    ### Key Changes:
    
    *   **Settings Theme Navigation:**
        *   Added `navigateToSettingsTheme()` extension function to `NavigationInterface`.
        *   This function wraps the navigation call to `SettingsThemeKey`, simplifying the usage in other parts of the app.

--- common/feature/settings/theme/src/main/kotlin/kz/rymbek/platform/common/feature/settings/theme/navigation/SettingsThemeNavigation.kt
+++ common/feature/settings/theme/src/main/kotlin/kz/rymbek/platform/common/feature/settings/theme/navigation/SettingsThemeNavigation.kt
@@ -3,10 +3,15 @@ package kz.rymbek.platform.common.feature.settings.theme.navigation
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import kotlinx.serialization.Serializable
+import kz.rymbek.platform.common.base.navigation.NavigationInterface
 import kz.rymbek.platform.common.feature.settings.theme.ui.SettingsThemeRoute
 
 @Serializable
 object SettingsThemeKey : NavKey
+
+fun NavigationInterface.navigateToSettingsTheme() {
+    navigate(SettingsThemeKey)
+}
 
 fun EntryProviderScope<NavKey>.settingsThemeScreen(
     onNavigateBack: () -> Unit,
