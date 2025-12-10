8064ea49a8325c75d953008504c960339f88a0ea
Rymbek Kimanov <91866986+Rym-bek@users.noreply.github.com>
2025-12-10 16:54:01 +0500

    Feat: Add IconWithText compound component
    This commit introduces a new compound UI component, `IconWithText`, designed to display an icon alongside a text label. This component leverages existing foundation elements to ensure consistent styling.
    
    **Key Changes:**
    
    *   **UI Components (`common/core/design/compound`):**
        *   Added `IconWithText.kt`: Implemented a Composable that aligns an `AppIcon` and `AppText` horizontally within an `AppRow`, supporting custom icon sizes.

---
+++ common/core/design/compound/src/main/kotlin/kz/rymbek/platform/common/core/design/compound/info/IconWithText.kt
@@ -0,0 +1,41 @@
+package kz.rymbek.platform.common.core.design.compound.info
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import kz.rymbek.platform.common.core.design.foundation.components.container.AppRow
+import kz.rymbek.platform.common.core.design.foundation.components.icon.AppIcon
+import kz.rymbek.platform.common.core.design.foundation.components.text.AppText
+import kz.rymbek.platform.common.core.design.foundation.constants.PlatformIconSize
+import kz.rymbek.platform.common.core.design.foundation.constants.PlatformPaddings
+
+@Composable
+fun IconWithText(
+    icon: ImageVector,
+    text: String,
+    iconSize: Dp = PlatformIconSize.xxs,
+) {
+    AppRow(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(PlatformPaddings.element),
+        modifier = Modifier
+            .padding(8.dp),
+        content = {
+            AppIcon(
+                imageVector = icon,
+                modifier = Modifier
+                    .size(iconSize),
+            )
+
+            AppText(
+                text = text,
+            )
+        }
+    )
+}
