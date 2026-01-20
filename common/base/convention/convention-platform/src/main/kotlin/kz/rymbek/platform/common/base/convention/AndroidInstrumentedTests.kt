da08cd2630e816a5d8e68e7d4a876d9b4f6a7678
Rymbek Kimanov <91866986+Rym-bek@users.noreply.github.com>
2026-01-20 11:12:51 +0500

    Fix: Correctly disable only Android instrumented tests
    This commit adjusts the logic in the `disableAllTests` extension function for `ApplicationAndroidComponentsExtension`.
    
    The previous implementation incorrectly disabled both unit and instrumented tests. The logic has been corrected to only disable Android instrumented tests by setting `it.androidTest.enable = false` for each variant, while the lines for disabling unit tests have been commented out.

--- common/base/convention/convention-platform/src/main/kotlin/kz/rymbek/platform/common/base/convention/AndroidInstrumentedTests.kt
+++ common/base/convention/convention-platform/src/main/kotlin/kz/rymbek/platform/common/base/convention/AndroidInstrumentedTests.kt
@@ -1,6 +1,5 @@
 package kz.rymbek.platform.common.base.convention
 
-import com.android.build.api.variant.AndroidComponentsExtension
 import com.android.build.api.variant.ApplicationAndroidComponentsExtension
 import com.android.build.api.variant.LibraryAndroidComponentsExtension
 
@@ -13,9 +12,9 @@ internal fun LibraryAndroidComponentsExtension.disableAllTests() {
 
 internal fun ApplicationAndroidComponentsExtension.disableAllTests() {
     return beforeVariants {
-        //it.androidTest.enable = false
-        (it as? com.android.build.api.variant.HasUnitTestBuilder)?.enableUnitTest = false
-        (it as? com.android.build.api.variant.HasAndroidTestBuilder)?.androidTest?.enable = false
+        it.androidTest.enable = false
+        //(it as? com.android.build.api.variant.HasUnitTestBuilder)?.enableUnitTest = false
+        //(it as? com.android.build.api.variant.HasAndroidTestBuilder)?.androidTest?.enable = false
         //it.unitTestEnabled = false
     }
 }
